14d5c8af07d58b91359d381027056d33d70484c5
sanskar sawant <sanskarsawant23@gmail.com>
2025-01-17 16:09:10 +0530

    added database

--- app/Component/Signup.tsx
+++ app/Component/Signup.tsx
@@ -1,16 +1,25 @@
+"use client"
 
+import { useState } from "react"
+import axios  from "axios";
 
 export function Signup(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handler = function (){
+        axios.post("http://localhost:3000/api/user", {username, password})
+    }
     return (
         <div className="flex flex-col h-screen justify-center ">
             <div className="flex justify-center">
-                <div>
-                <input type="text" placeholder="Username" className="p-2 m-2"/>
+                <div className="border rounded pb-4">
+                <input type="text" placeholder="Username" className="p-2 m-2 border border-black-400" onChange={((e)=>setUsername(e.target.value))}/>
                 <br />
-                <input type="text" placeholder="Password" className="p-2 m-2" />
+                <input type="password" placeholder="Password" className="p-2 m-2 border border-black-400" onChange={((e)=>setPassword(e.target.value))} />
                 <br />
-           <div className="flex justify-center">
-            <button className=" bg bg-blue-300">Sign up</button>
+           <div className="flex justify-center mt-4 " >
+            <button onClick={handler} className=" bg bg-blue-300 px-3 border rounded py-2">Sign up</button>
            </div>
             
             </div>
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
 
 
 
@@ -9,10 +12,30 @@ export function GET(){
 }
 
 export async function POST(req: NextRequest){
-    const body =  req.json 
-    console.log(req.headers.get("authorization"));
-    return NextResponse.json({
-        msg:"you have sent the data successfully"
-    })
+   const body = await req.json();
+   //add the email and password to the database
+   try{
+    const user = await prisma.user.create({
+        data:{
+            email:body.username,
+            password:body.password
+        }
+   })
+   return NextResponse.json({
+    msg:"entry to database done",
+    body
+   })
 
+   }
+   catch(error){
+        NextResponse.json({
+            message:"Error white signing up"
+        }, {
+            status:411
+        })
+   }
+  
+   return NextResponse.json({
+    body
+   })
 }
